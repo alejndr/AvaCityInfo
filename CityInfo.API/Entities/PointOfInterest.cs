923055f5109bf9de27c95e7fbfe2ce77bb449d10
I. la Maza Villalba <i.la.maza.villalba@avanade.com>
2019-01-08 12:12:59 +0100

    Added creating database and populate functions

--- CityInfo.API/Entities/PointOfInterest.cs
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -17,6 +17,9 @@ namespace CityInfo.API.Entities
 		[MaxLength(50)]
 		public string Name { get; set; }
 
+		[MaxLength(200)]
+		public string Description { get; set; }
+
 		// (Esta sera nuestra foreign key)
 		[ForeignKey("CityId")]
 		public City City { get; set; }
